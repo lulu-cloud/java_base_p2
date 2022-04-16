d3302760b6e3ca97420f674f90eaecfc0467d940
Lili <xulu_lili@163.com>
2022-04-16 17:45:55 +0800

    second commit about some java code;

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Go to nwpu follow panwei");
+        System.out.println("Hello Git2!");
     }
 }
